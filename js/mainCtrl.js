5bb0360f9ed503536901990c6bcce2227bb2c772
MikeB <burton.mj@gmail.com>
2017-02-07 11:15:31 -0700

    tinkering

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,10 +1,16 @@
 angular.module('friendsList').controller('mainCtrl', function($scope){
 $scope.friends = [
-  'Heidi', 'Scotty', 'DJ', 'Michael'
+  { name: "Heidi Hawkins", relationship: "wife"},
+  { name: "Scotty John", relationship: "friend"},
+  { name: "Kris Burton", relationship: "mom"},
+  { name: "DJ Shooter", relationship: "business associate"},
 ];
 
 $scope.addFriend = function(){
-  $scope.friends.push($scope.newFriend);
-}
-
+  $scope.friends.push(
+    {
+      name: $scope.newFriend.name,
+      relationship: $scope.newFriend.relationship
+    });
+  };
 });
